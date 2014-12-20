8fcfe4fc9147642080585ab944d12f7284a2d72b
mgontav <mgontav@gmail.com>
2014-12-20 05:30:26 +0000

    Node client writes to serial the proper sequences.

--- client.js
+++ client.js
@@ -1,21 +1,62 @@
-var socket = require('socket.io-client')('http://interactions.whitesmith.co:80/office');
-var light = {};
+var SerialPort = require("serialport").SerialPort
 
-socket.on('connect', function(){
-	console.log("Connected to Whitesmith's socket server.")
-	console.log("Please behave.")
-});
+var serialPort = new SerialPort("tty_terminal");
+var command = "0";
 
-socket.on('light:setup', changeLight);
-socket.on('light:on',    changeLight);
-socket.on('light:off',   changeLight);
-socket.on('light:color', changeLight);
+serialPort.on("open", function () {
+  	console.log('Serial Port open');
+  	console.log('Opening socket to WS interactions.')
+  	var socket = require('socket.io-client')('http://interactions.whitesmith.co:80/office');
 
-socket.on('disconnect', function(){
-	console.log("Disconnected.")
+  	socket.on('connect', function(){
+		console.log("Connected to Whitesmith's socket server.")
+		console.log("Please behave.")
+  	});
+
+  	socket.on('light:setup', changeLight);
+	socket.on('light:on',    changeLight);
+	socket.on('light:off',   changeLight);
+	socket.on('light:color', changeLight);
+
+  	socket.on('disconnect', function(){
+		console.log("Disconnected from WS interactions.")
+	});
 });
 
 function changeLight( state ){
-	console.log("Something has changed. Let's act accordingly.")
+	console.log("Light has been changed.")
 	console.log(state);
+
+	if(state.on == false){
+		command = "0";
+	}else{
+		switch(state.color.r){
+			case 241: //Yellow
+				command = "1";
+				break;
+			case 243: //Orange
+				command = "2";
+				break;
+			case 231: //Red
+				command = "3";
+				break;
+			case 26: //Cyan
+				command = "4";
+				break;
+			case 41: //Blue
+				command = "5";
+				break;
+			case 46: //Green
+				command = "6";
+				break;
+			case 155: //Purple
+				command = "7";
+				break;
+		}
+	}
+
+	serialPort.write(command, function(err, results) {
+    	console.log('err ' + err);
+  	  console.log('results ' + results);
+  	});
 }
